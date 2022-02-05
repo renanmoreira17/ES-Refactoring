23df66f4a00c69f3701312b727f7ba03ff698bb2
Renan Moreira <renanmoreira17@gmail.com>
2022-02-05 14:50:03 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,30 +1,15 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = getHeaderText(aCustomer.getName());
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // show figures for each rental
-            result += getRentalText(each);
-        }
-        // add footer lines
-        result += getFooterText(aCustomer);
-        return result;
-    }
-
-    private String getHeaderText(String customerName) {
+    protected String getHeaderText(String customerName) {
         return "<H1>Rentals for <EM>" + customerName +
                 "</EM></H1><P>\n";
     }
 
-    private String getRentalText(Rental rental) {
+    protected String getRentalText(Rental rental) {
         return rental.getMovie().getTitle() + ": " +
                 String.valueOf(rental.getCharge()) + "<BR>\n";
     }
 
-    private String getFooterText(Customer customer) {
+    protected String getFooterText(Customer customer) {
         String result = "";
         result += "<P>You owe <EM>" +
                 String.valueOf(customer.getTotalCharge()) + "</EM><P>\n";
