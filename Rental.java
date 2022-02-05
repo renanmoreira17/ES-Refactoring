c1a4defc9615fb77ee42bf0cace733b48d518bf2
Renan Moreira <renanmoreira17@gmail.com>
2022-02-04 22:15:13 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -37,4 +37,12 @@ public class Rental {
 
         return thisAmount;
     }
+
+    public int getFrequentRenterPoints() {
+        if (this.getMovie().getPriceCode() == Movie.NEW_RELEASE && this.getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
