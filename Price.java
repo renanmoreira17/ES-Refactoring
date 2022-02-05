63cb96057da5d12089377268084dd8a09b29ef55
Renan Moreira <renanmoreira17@gmail.com>
2022-02-05 14:16:25 -0300

    Commit 14

--- Price.java
+++ Price.java
@@ -2,4 +2,12 @@ public abstract class Price {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if (this.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
