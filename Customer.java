6a5da783d801d78ee163276a71b4b2e35452a8b5
Renan Moreira <renanmoreira17@gmail.com>
2022-02-04 22:10:20 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -24,8 +24,6 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = each.getCharge();
-            totalAmount += thisAmount;
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -36,8 +34,8 @@ public class Customer {
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
